d933aa29702d8e40889a5b28168c38f6b0ee8d17
Ishanh2000 <misraishanh200@gmail.com>
2021-11-16 01:27:44 +0530

    Minor changes

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 # ssh -i "698.pem" ubuntu@ec2-18-222-200-30.us-east-2.compute.amazonaws.com
 from flask_pymongo import PyMongo
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import json
 from bson import json_util
 from infer import face_labels
@@ -11,60 +12,62 @@ IMG_PATH = "./img/"
 DAY = 86400 # length of a day in seconds
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = "Content-Type"
+
 mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/scove")
 db = mongodb_client.db
 
-@app.route("/add_one")
-def add_one():
-  db.todos.insert_one({'title': "todo title", 'body': "todo body"})
-  return jsonify(message="success")
 
-@app.route("/events", methods=["GET", "POST", "DELETE"])
+@app.route("/")
+@cross_origin()
+def hello_world():
+  return "Hello, World!"
+
+
+#### EVENTS ####
+@app.route("/events", methods=["POST"])
+@cross_origin()
 def events():
-  if request.method == "GET":
-    return "GETTER"
+  if request.method != "POST": return "Bad Request", 400
+  try:
+    data = None
+    for f in request.files:
+      if f != "json": request.files[f].save(IMG_PATH + f)
+      else: data = json.loads(request.files["json"].read()) # 'start', 'phase', 'isMischief', 'images'
 
-  if request.method == "POST":
-    try:
-      data = None
-      for f in request.files:
-        if f != "json": request.files[f].save(IMG_PATH + f)
-        else: data = json.loads(request.files["json"].read()) # 'start', 'phase', 'isMischief', 'images'
+    x = db.events.find_one({ "start" : data["start"] })
+    if x == None:
+      db.events.insert_one({
+        "start" : data["start"],
+        f"phase_{data['phase']}" : { "isMischief" : data["isMischief"], "images" : data["images"] }
+      })
+    else:
+      db.events.update_one( { "_id" : x["_id"] }, { "$set" : {
+        f"phase_{data['phase']}" : { "isMischief" : data["isMischief"], "images" : data["images"] }
+      } }, upsert=False)
+
+    # if phase 2 - take optimistic attendance if not isMischief
+    if (data["phase"] == 2) and not data["isMischief"]:
+      db.opt_att.insert_one({
+        "start" : data["start"],
+        "attTime" : data["images"][-1]["time"],
+        "faceLabel" : data["images"][-1]["status"]["faceLabel"],
+      })
+
+    # if phase 3 - delete optimistic attendance if isMischief
+    if (data["phase"] == 3) and data["isMischief"]:
+      db.opt_att.remove({ "start" : data["start"] })
       
+    return "SUCCESS"
 
-      x = db.events.find_one({ "start" : data["start"] })
-      if x == None:
-        db.events.insert_one({
-          "start" : data["start"],
-          f"phase_{data['phase']}" : { "isMischief" : data["isMischief"], "images" : data["images"] }
-        })
-      else:
-        db.events.update_one( { "_id" : x["_id"] }, { "$set" : {
-          f"phase_{data['phase']}" : { "isMischief" : data["isMischief"], "images" : data["images"] }
-        } }, upsert=False)
-
-      # if phase 2 - take optimistic attendance if not isMischief
-      if (data["phase"] == 2) and not data["isMischief"]:
-        db.opt_att.insert_one({
-          "start" : data["start"],
-          "attTime" : data["images"][-1]["time"],
-          "faceLabel" : data["images"][-1]["status"]["faceLabel"],
-        })
-
-      # if phase 3 - delete optimistic attendance if isMischief
-      if (data["phase"] == 3) and data["isMischief"]:
-        db.opt_att.remove({ "start" : data["start"] })
-        
-      return "SUCCESS"
-
-    except:
-      return "ERROR"
-
-  if request.method == "DELETE":
-    return "DELETER"
+  except:
+    return "ERROR"
 
 
-@app.route("/attendance", methods=["GET", "POST", "DELETE"])
+#### ATTENDANCE ####
+@app.route("/attendance", methods=["GET", "POST"])
+@cross_origin()
 def attendance():
   if request.method == "GET":
     args = dict(request.args)
@@ -97,22 +100,27 @@ def attendance():
   if request.method == "POST":
     try:
       data = request.json # 'start', 'attTime', 'faceLabel', 'avgTemp', 'maskLabel', 'disallowReason'
-      
-      # delete optimistic attendance
-      db.opt_att.remove({ "start" : data["start"] })
-
-      # take real attendance
-      db.att.insert_one(data)
-
+      db.opt_att.remove({ "start" : data["start"] }) # delete optimistic attendance
+      db.att.insert_one(data) # take real attendance
       return "SUCCESS"
 
     except:
       return "ERROR"
 
-  if request.method == "DELETE":
-    return "DELETER"
 
+#### REGISTER ####
+@app.route("/register", methods=["POST"])
+@cross_origin()
+def register():
+  if request.method != "POST": return "Bad Request", 400
+  # N = students, M images per student
+  # { "train" : [
+  #   { "name" : "imisra", "files" : ['..', '..'] },
+  #   { "name" : "tulika", "files" : ['..', '..'] },
+  # ] }
+  train = request.json.get("train")
+  if not train: return "Expected field \"train\" - an array", 400
+  # link face_register/main.py as train.py
 
-@app.route("/")
-def hello_world():
-  return "Hello, World!"
+  
+  return "OK"
